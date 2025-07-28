989cb7931d24753186c3f907fe2691454e3a6583
Felipe <rocha.felipe98@gmail.com>
2025-07-28 18:01:03 -0300

    update cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://warranty-manager-frontend.onrender.com', 'http://localhost:5173'
+    origins 'https://warranty-manager-frontend.onrender.com', 'http://127.0.0.1:5173'
 
     resource '*',
       headers: :any,
